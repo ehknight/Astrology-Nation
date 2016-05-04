f61d3c44c522caa75d627028e5f452c3ac23ebca
hyperdo <super.ethan@gmail.com>
2016-05-03 22:04:48 -0700

    reverse shellification

--- main.py
+++ main.py
@@ -11,4 +11,6 @@ def stupidcss():
     return render_template('stupid.css')
 
 if __name__=='__main__':
-    app.run(port=80, debug = True)
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port, debug = True)
+
